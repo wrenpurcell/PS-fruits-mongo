24cadbd4c895208102e4f0590fcd4367831c4209
Wren Purcell <purcell856@gmail.com>
2022-03-04 11:41:08 -0500

    removed unnessecary code

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const fruits = require('./models/fruits.js'); 
 
-//set up view engine
+//set up view engine above routes
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+
+//index route
+app.get("/fruits", function (req, res) {
+  res.render("Index", { fruits: fruits });
+}); 
 
 app.get('/fruits/:indexOfFruitsArray', function(req, res){
   res.render('Show', {//second param must be an object
@@ -12,19 +17,10 @@ app.get('/fruits/:indexOfFruitsArray', function(req, res){
     //there will be a variable available inside the ejs file called fruit, its value is fruits[req.params.indexOfFruitsArray]
   })
 })
-//index route
-app.get("/fruits", function (req, res) {
-  res.render("Index", { fruits: fruits });
-}); 
 
-app.get("/fruits/", (req, res) => {
-  res.send(fruits);
-});
 
-// app.get("/fruits/:indexOfFruitsArray", (req, res) => {
-//   res.send(fruits[req.params.indexOfFruitsArray]);
-// });
 
-app.listen(3001, () => {
+
+app.listen(3000, () => {
   console.log("listening");
 });
